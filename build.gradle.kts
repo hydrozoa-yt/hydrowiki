1dbcfeb4a72282b710b45dac35eff7c011d7f5e0
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-22 18:47:50 +0200

    added flexmark dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("org.freemarker:freemarker:2.3.34")
     implementation("com.google.guava:guava:33.4.8-jre")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.5.3")
+    implementation("com.vladsch.flexmark:flexmark-all:0.64.8")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
