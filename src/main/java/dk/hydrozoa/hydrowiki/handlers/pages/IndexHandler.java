d25b29d5b7b58414c4d58c5652cbcacbdeba34f1
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-22 12:33:22 +0200

    remove dead code

--- src/main/java/dk/hydrozoa/hydrowiki/handlers/pages/IndexHandler.java
+++ src/main/java/dk/hydrozoa/hydrowiki/handlers/pages/IndexHandler.java
@@ -6,29 +6,14 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.util.Callback;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
 public class IndexHandler extends IHandler {
 
-    String indexText;
-
-    public IndexHandler(String path) {
-        try {
-            this.indexText = Files.readString(Path.of("data", "index.html"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
-    public boolean handle(Request request, Response response, Callback callback) throws Exception {
-        Map model = new HashMap();
-        String content = Templater.renderTemplate("index.ftl", model);
-        String fullPage = Templater.renderBaseTemplate("Index page title", content);
+    public boolean handle(Request request, Response response, Callback callback) {
+        String content = Templater.renderTemplate("index.ftl", Map.of());
+        String fullPage = Templater.renderBaseTemplate("HydroWiki", content);
         sendHtml(200, fullPage, response, callback);
         return true;
     }
