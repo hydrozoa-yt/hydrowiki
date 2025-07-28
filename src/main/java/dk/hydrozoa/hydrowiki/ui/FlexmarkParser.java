72adbc3b400cc97ee53cdc543e8317438affcc62
hydrozoa <hydrozoa.rs@gmail.com>
2025-07-28 15:01:22 +0200

    add todos

--- src/main/java/dk/hydrozoa/hydrowiki/ui/FlexmarkParser.java
+++ src/main/java/dk/hydrozoa/hydrowiki/ui/FlexmarkParser.java
@@ -4,6 +4,9 @@ import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 
+// todo add visitor to headings and change their levels
+// todo configure wikilink plugin
+// todo configure image plugin
 public class FlexmarkParser {
 
     public String parse(String input) {
